2b8d2d81d508a54ec414e84301c1016bcad8d141
Your Name <you@example.com>
2021-05-31 23:11:54 +0100

    get all users setup

--- api/server.js
+++ api/server.js
@@ -1,3 +1,26 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const User = require('./users/model')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+
+server.use(express.json())
+
+server.get('/hello', (req, res) =>{
+    res.json({message: 'hello' })
+    })
+
+server.get('/api/users', async (req, res) =>{
+    try {
+        const users = await User.findAll()
+        res.json(users)
+    } catch(err) {
+        res.status(500).json({ 
+        message: 'The users information could not be retrieved',
+        error: err.message,
+    }) 
+    }
+})
+
+module.exports = server; 
+// EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const server = require('./api/server');
 
-const port = 5000;
-
 // START YOUR SERVER HERE
+server.listen(5000, () => {
+    console.log ('server listening on 5000')
+})

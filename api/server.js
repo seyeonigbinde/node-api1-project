6cd22f4f01659bea98c545bafb98cf42f34c9901
Your Name <you@example.com>
2021-06-01 02:05:26 +0100

    put request to server successful

--- api/server.js
+++ api/server.js
@@ -74,5 +74,27 @@ server.delete('/api/users/:id', async (req, res) =>{
             })
         })
     }) 
+
+
+server.put('/api/users/:id', async (req, res) =>{
+        const {id} = req.params
+        const {body} = req
+        try {
+            const updated = await User.update(id, body)
+            if (!updated) {
+                res.status(404).json({
+                    message: `The user with the ID ${id} does not exist`
+                })
+            } else {
+                res.json(updated)
+                
+            }
+        }catch(err) {
+            res.status(500).json({ 
+            message: 'The user information could not be modified',
+            error: err.message,
+            }) 
+        }
+    }) 
 module.exports = server; 
 // EXPORT YOUR SERVER instead of {}

098be646f1a0cfd91532a0dc4e14eb4027869790
Your Name <you@example.com>
2021-06-03 23:11:11 +0100

    heroku deployment

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
+// const server = require('./api/server');
+
+// // START YOUR SERVER HERE
+// server.listen(6000, () => {
+//     console.log ('server listening on 6000')
+// })
+
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+
+// const server = express()
 const server = require('./api/server');
 
-// START YOUR SERVER HERE
-server.listen(6000, () => {
-    console.log ('server listening on 6000')
+server.use(express.json())
+server.use(cors())
+// server.use(express.static(path.join(__dirname, 'client/build'))) // static assets
+
+console.log(process.env.USER) 
+console.log(process.env.SHELL)
+
+if (process.env.NODE_ENV === 'production') {
+  console.log('this means this code is deployed')
+}
+
+const PORT = process.env.PORT || 6000
+
+console.log('port is -> ', PORT)
+
+// server.get('/', (req, res) => {
+//   // sending back index.html
+//   res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+// })
+
+server.get('/api', (req, res) => {
+  res.json({ message: `${process.env.COHORT} ROCKS` })
+})
+
+server.use((req, res) => {
+  res.status(404).json({ message: 'not found sorry' })
+})
+
+server.listen(PORT, () => {
+  console.log(`listening on ${PORT}`)
 })
